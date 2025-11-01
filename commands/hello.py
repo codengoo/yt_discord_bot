eef6079e16127f7460fb15c8d0381452f314a3bc
NghiaDT <nghiadt22.work@gmail.com>
2025-11-01 21:21:55 +0700

    update

--- commands/hello.py
+++ commands/hello.py
@@ -3,4 +3,4 @@ from discord.ext import commands
 def setup_hello(bot):
     @bot.command(name="hello")
     async def hello(ctx):
-        await ctx.send("👋 Hello! Bot đang hoạt động nè đồ ngu")
+        await ctx.send("👋 Hello! Bot đang hoạt động nè đồ ngu!")
